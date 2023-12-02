a99275f984ab4297a8af1d4a884d58ecaec653f7
maksim <mkotelnikov97@gmail.com>
2023-12-02 23:22:56 +0300

    Fix: bug with sending messages to different users

--- bot/bot.go
+++ bot/bot.go
@@ -5,19 +5,17 @@ import (
 	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/nfnt/resize"
-	qrcode "github.com/skip2/go-qrcode"
+	"github.com/skip2/go-qrcode"
 	"image"
 	"image/draw"
 	"image/jpeg"
 	"log"
 	"net/url"
-	_ "os"
 )
 
 const TOKEN = "6821853023:AAFDhL3b8cEWKbo7sgkLZVYP8lLEZGAa-nQ"
 
 func main() {
-	var err error
 	bot, err := tgbotapi.NewBotAPI(TOKEN)
 	if err != nil {
 		log.Fatal(err)
@@ -28,77 +26,60 @@ func main() {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 	updates := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	for update := range updates {
 		if update.Message == nil {
 			continue
 		}
 
-		switch update.Message.Text {
-		case "/start":
+		handleMessage(bot, update.Message)
+	}
+}
 
-			bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Привет, "+update.Message.From.FirstName+"! Добро пожаловать в бота который создаст QR-код для твоей ссылки."))
-			bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Просто отправь мне свою ссылку и в ответ ты получишь QR-Code на свой URL."))
+func handleMessage(bot *tgbotapi.BotAPI, msg *tgbotapi.Message) {
+	switch msg.Text {
+	case "/start":
+		bot.Send(tgbotapi.NewMessage(msg.Chat.ID, "Привет, "+msg.From.FirstName+"! Добро пожаловать в бота, который создаст QR-код для твоей ссылки."))
+		bot.Send(tgbotapi.NewMessage(msg.Chat.ID, "Просто отправь мне свою ссылку и в ответ ты получишь QR-Code на свой URL."))
 
-			for {
-				userURLMessage := <-updates
-				userURL := userURLMessage.Message.Text
-				fmt.Println(userURL)
+	default:
+		handleUserURL(bot, msg)
+	}
+}
 
-				if userURL != "/start" && checkURL(userURL) {
-					jpegData, err := createQRCode(userURL)
-					if err != nil {
-						log.Fatal(err)
-					}
+func handleUserURL(bot *tgbotapi.BotAPI, msg *tgbotapi.Message) {
+	userURL := msg.Text
 
-					photoConfig := tgbotapi.PhotoConfig{
-						BaseFile: tgbotapi.BaseFile{
-							BaseChat: tgbotapi.BaseChat{
-								ChatID: update.Message.Chat.ID,
-							},
-							File: tgbotapi.FileBytes{
-								Name:  "qr.jpg",
-								Bytes: jpegData,
-							},
-						},
-						Caption: "Ваш QR-Code по запросу.", // Опциональный заголовок
-					}
-
-					_, err = bot.Send(photoConfig)
-					if err != nil {
-						log.Fatal(err)
-					}
-				} else if checkURL(userURL) {
-					fmt.Println("true url")
-
-					jpegData, err := createQRCode(userURL)
-					if err != nil {
-						log.Fatal(err)
-					}
-
-					photoConfig := tgbotapi.PhotoConfig{
-						BaseFile: tgbotapi.BaseFile{
-							BaseChat: tgbotapi.BaseChat{
-								ChatID: update.Message.Chat.ID,
-							},
-							File: tgbotapi.FileBytes{
-								Name:  "qr.jpg",
-								Bytes: jpegData,
-							},
-						},
-						Caption: "Ваш QR-Code по запросу.", // Опциональный заголовок
-					}
-
-					_, err = bot.Send(photoConfig)
-					if err != nil {
-						log.Fatal(err)
-					}
-
-				} else {
-					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Неправильный URL, попробуй еще раз"))
-				}
-			}
+	if userURL != "/start" && checkURL(userURL) {
+		jpegData, err := createQRCode(userURL)
+		if err != nil {
+			log.Println(err)
+			return
 		}
+
+		photoConfig := tgbotapi.PhotoConfig{
+			BaseFile: tgbotapi.BaseFile{
+				BaseChat: tgbotapi.BaseChat{
+					ChatID: msg.Chat.ID,
+				},
+				File: tgbotapi.FileBytes{
+					Name:  "qr.jpg",
+					Bytes: jpegData,
+				},
+			},
+			Caption: "Ваш QR-Code по запросу.",
+		}
+
+		_, err = bot.Send(photoConfig)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	} else {
+		bot.Send(tgbotapi.NewMessage(msg.Chat.ID, "Неправильный URL, попробуй еще раз"))
 	}
 }
 
